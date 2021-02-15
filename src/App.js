5246be9755951d6e00faf195502f29893d421b77
littwa <littwa777@gmail.com>
2021-02-15 23:57:58 +0200

    upgr2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 
 import Main from "./Comonents/Main";
 
-// function App() {
-//   return <Main />;
-// }
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/score" component={() => <div>Score</div>} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Main />;
 }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/" exact component={Main} />
+//         <Route path="/score" component={() => <div>Score</div>} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
